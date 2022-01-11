b084a23be280db42a8251a7920f2aabf6635068c
Elias <eliassilva0045@gmail.com>
2022-01-10 21:51:48 -0300

    Cria game Rock Paper Scissors in Kotlin

---
+++ src/Game.kt
@@ -0,0 +1,43 @@
+fun main(){
+    val options = arrayOf("Rock","Paper","Scissors")
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserChice(options)
+//    for ((i, v) in options.withIndex()) println("No index $i está $v")
+}
+
+fun getGameChoice(optionsParam: Array<String>): String = optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>): String{
+    var isValidChoice = false
+    var userChoice = ""
+    //Loop until the user enters a valid choice
+    while (!isValidChoice){
+        // Ask the user for their choice
+        print("Please enter one of the following: ")
+        for(item in optionsParam) println("$item")
+        println(".")
+        // Read the user input
+        val userInput = readLine()
+        // Validate the user input
+        if(userInput != null && userInput in optionsParam){
+            isValidChoice = true
+            userChoice = userInput
+        }
+        // if the choice is invalid, inform the user
+        if (!isValidChoice) println("You must enter a valid choice.")
+    }
+    return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String){
+    val result: String
+    if(userChoice == gameChoice) result = "Tie!"
+    else if (
+        (userChoice == "Rock" && gameChoice == "Scissors") ||
+        (userChoice == "Paper" && gameChoice == "Rock") ||
+        (userChoice == "Scissors" && gameChoice == "Paper" )
+    ) result = "You win!"
+    else result = "You lose!"
+    // Print the result
+}
+
